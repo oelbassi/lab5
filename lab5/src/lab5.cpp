c4a64a71664f83f565144ea20deab35a88242c3d
oelbassi <oelbassi@ryerson.ca>
2015-10-26 16:58:12 -0400

    added another line

---
+++ lab5/src/lab5.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : lab5.cpp
+// Author      : me
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "change 1"<< endl;
+	cout<<"my name is omar"<<endl;
+	cout <<"changes"<<endl;
+	cout<<"change2"<<endl;
+	cout <<"another change?"<<endl;
+	cout <<"line"<<endl;
+
+
+
+	return 0;
+}
